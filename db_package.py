01cbe5c8047148af6b56db2df1c0e3b63393f973
seetler <jsnzeng@gmail.com>
2024-10-15 20:56:29 -0700

    commenting

--- db_package.py
+++ db_package.py
@@ -8,19 +8,17 @@ import os
 
 client = openai.OpenAI(api_key=key_api_key0)
 
-
-
-# This functions runs a thread. The purpose of having this function is to have a way to insert variables into the OpenAI client.beta.threads.runs.create_and_poll function.
+# This functions creates a thread
 def func_create_thread():
-    # this is setup where this is the function, however the outer shell allows insertion of thread ID, assistant ID, and instructions.
+
     run_id = client.beta.threads.create()
     
     return run_id.id
 
 
-
-# This functions adds a user message to a thread.
+# This functions adds a user message to the thread.
 def func_send_msg(user_input0, var_thread_id0):
+    
     client.beta.threads.messages.create(
 
         thread_id=var_thread_id0,
@@ -30,11 +28,9 @@ def func_send_msg(user_input0, var_thread_id0):
     )
 
 
-
-# This functions runs a thread. The purpose of having this function is to have a way to insert variables into the OpenAI client.beta.threads.runs.create_and_poll function.
+# This functions runs a thread.
 def func_run_thread(form_county, var_thread_id0):
-    
-    # this is setup where this is the function, however the outer shell allows insertion of thread ID, assistant ID, and instructions.
+
     run = client.beta.threads.runs.create_and_poll(
 
         thread_id=var_thread_id0,
@@ -43,10 +39,7 @@ def func_run_thread(form_county, var_thread_id0):
     )
 
 
-
-
-
-# This retries the latest message after the thread is run
+# This retrieves the latest message after the thread is run
 def func_retrieve_msg(var_thread_id0):
     messages = client.beta.threads.messages.list(
         
@@ -57,25 +50,17 @@ def func_retrieve_msg(var_thread_id0):
     return messages.data[0].content[0].text.value
 
 
-
-
-
 # This cleans the file.
 def text_fix(text):
+
     # Replace double asterisks (**) with <strong> for bold text
     html_text = re.sub(r'\*\*(.*?)\*\*', r'<strong>\1</strong>', text)
-    
     # Replace newline characters (\n) with <br> for line breaks
     html_text = html_text.replace('\n', '<br>')
-    
     # Remove any text between 【 and 】
     html_text = re.sub(r'【.*?】', '', html_text)
     
     return html_text
-
-
-
-
 
 
 # This runs the full cycle in one function.
