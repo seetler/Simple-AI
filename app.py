709e06cd2b92d0521385f98008e16dde9bd464ba
seetler <jsnzeng@gmail.com>
2024-10-15 20:35:15 -0700

    0.4.0 so many small bugs.

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ from datetime import datetime
 app = Flask(__name__)
 
 
-
 #Base home page.
 @app.route('/')
 def home():
@@ -14,6 +13,8 @@ def home():
 
 global_thread = "threads/thread_wqrYgWSOoth0ZYf3YeIov9Jx"
 global_county = "Sonoma"
+
+
 
 #When the route is /submit, it posts the information. It takes, input_user and category
 @app.route('/submit', methods=['POST', 'GET'])
@@ -27,90 +28,65 @@ def submit():
     #creates a thread
     var_thread_id0=(client.beta.threads.create()).id
     global_thread = var_thread_id0
+    
     print(var_thread_id0)
 
 
     #puts in prompts
-    
     subm_prompt = request.form.get('form_prompt')
     if subm_prompt is None or subm_prompt.strip() == "":
-        subm_prompt = "Can I camp in parks? Are there fees?"
+        subm_prompt = "What is the fine for littering in public?"
     
     subm_email = request.form.get('form_email')
     if subm_email is None or subm_email.strip() == "":
         subm_email = "null"
 
     subm_county=request.form.get('form_county')
-
-
-
     global_county = subm_county
+
     timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
-
-    with open('email.txt', 'a') as f:
-        f.write(f'{timestamp} - Email: {subm_email} \n')
-
+    output_0=f'{timestamp} - Thread: {global_thread} | - Question: {subm_prompt} | County: {global_county}\n'
     with open('log.txt', 'a') as f:
-        f.write(f'{timestamp} - Email: {subm_email} \n')
-
+        f.write(output_0)
     
+    # Calls the thread, then writes the reponse.
+    response_prompt = func_complete_cycle(subm_prompt, global_county, global_thread)    
+    response_0=f'{timestamp} - Thread: {global_thread} | - Response: {repr(response_prompt)} | County: {global_county}\n'
     with open('log.txt', 'a') as f:
-        f.write(f'{timestamp} - Thread: {global_thread} |  - Question: {subm_prompt} | County: {subm_county}\n')
+        f.write(response_0)   
     
-    print(assistant_dict[subm_county])
-    print(subm_prompt)
-
-    response_prompt = func_complete_cycle(subm_prompt, subm_county, var_thread_id0)
-    
-    with open('log.txt', 'a') as f:
-        f.write(f'{timestamp} - Thread: {global_thread} | Question: {repr(response_prompt)} | County: {subm_county}\n')   
-    
-    print(repr(response_prompt))
     return render_template('conversation.html', response_prompt=response_prompt, response_county=subm_county)
 
 
 
 
 
-#When the route is /submit, on the followup
+#When the route is /conversation, on the followup
 @app.route('/conversation', methods=['POST'])
 def conversation():
 
     global global_thread, global_county
 
-
-
     subm_prompt = request.form.get('form_prompt')
     if subm_prompt is None or subm_prompt.strip() == "":
         subm_prompt = "followup"
 
-
-  #puts in prompts
-
     timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
-    with open('log.txt', 'a') as f:
-        f.write(f'{timestamp} - Thread: {global_thread} |  - Question: {subm_prompt} | County: {global_county}\n')
-    
-    print(assistant_dict[global_county])
-    print(subm_prompt)
+    output_0=f'{timestamp} - Thread: {global_thread} |  - Question: {subm_prompt} | County: {global_county}\n'
 
-    response_prompt = func_complete_cycle(subm_prompt, global_county, global_thread)
-    
     with open('log.txt', 'a') as f:
-        f.write(f'{timestamp} - Thread: {global_thread} | Question: {repr(response_prompt)} | County: {global_county}\n')   
-    
-    print(repr(response_prompt))
+        f.write(output_0)
+
+
+    response_prompt = func_complete_cycle(subm_prompt, global_county, global_thread)    
+    response_0=f'{timestamp} - Thread: {global_thread} | Question: {repr(response_prompt)} | County: {global_county}\n'
+    with open('log.txt', 'a') as f:
+        f.write(response_0)   
+
     return render_template('conversation.html', response_prompt=response_prompt, response_county=global_county)
 
-
-
-
-#Base home page.
-@app.route('/greet')
-def greet():
-    return render_template('greet.html')
 
 
 if __name__ == '__main__':
