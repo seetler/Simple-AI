be2a98dae6f32b073516f9509f5900b24fcc832d
seetler <jsnzeng@gmail.com>
2025-03-03 22:41:09 -0800

    0.2.1

--- appv020.py
+++ appv020.py
@@ -38,4 +38,4 @@ def chat():
     return Response(stream_openai_response(prompt), content_type='text/plain')
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(debug=True, port=portto)
